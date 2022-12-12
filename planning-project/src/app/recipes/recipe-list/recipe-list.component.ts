a52b76e01407e46eb286e030383cc6fff49a87eb
Guilherme Santos <sntguilherme@gmail.com>
2022-12-12 10:25:01 +0100

    planning project first part

---
+++ planning-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { Recipe } from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe("A test recipe", "this is simple a test", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F03%2F03%2F7782449.jpg"),
+    new Recipe("A test recipe", "this is simple a test", "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F03%2F03%2F7782449.jpg")
+  ];
+}
