8f7cac1347d5874a0eb2c2ae93112710586f63a0
Guilherme Santos <sntguilherme@gmail.com>
2022-12-13 14:36:39 +0100

    services and dependency injection

---
+++ services/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from "./account.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  accounts: { name: string, status: string }[] = [];
+
+  constructor(private accountService: AccountService) {
+  }
+
+  ngOnInit() {
+    this.accounts = this.accountService.accounts;
+  }
+
+  onAccountAdded(newAccount: { name: string, status: string }) {
+    // this.accounts.push(newAccount);
+  }
+
+  onStatusChanged(updateInfo: { id: number, newStatus: string }) {
+    // this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  }
+}
