f3ad919ece1173ab6cdda4912d38a933b8f4491d
Guilherme Santos <sntguilherme@gmail.com>
2022-12-13 17:39:37 +0100

    routing

---
+++ routing/src/app/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class AuthService {
+  loggedIn = false;
+
+  isAuthenticated() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.loggedIn);
+      }, 1000);
+    });
+  }
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+}
