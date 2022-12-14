3b9c492c8791c2cccafd78df4767473a1e4b6ae1
Guilherme Santos <sntguilherme@gmail.com>
2022-12-14 10:50:52 +0100

    planning project with observables in the services

--- planning-project/src/app/app.component.ts
+++ planning-project/src/app/app.component.ts
@@ -6,11 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'planning-project';
-  loadedFeature = 'recipes';
-
-  onNavigate(feature: string) {
-    console.log(feature);
-    this.loadedFeature = feature;
-  }
 }
--- planning-project/src/app/shopping-list/shopping-list.service.ts
+++ planning-project/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,11 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
+import { Subject } from 'rxjs';
+
 import { Ingredient } from "../shared/ingredient.model";
 
 @Injectable()
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient("Apples", 5),
     new Ingredient("Tomatoes", 10),
@@ -15,11 +17,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit([...this.ingredients]);
+    this.ingredientsChanged.next([...this.ingredients]);
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit([...this.ingredients]);
+    this.ingredientsChanged.next([...this.ingredients]);
   }
 }
