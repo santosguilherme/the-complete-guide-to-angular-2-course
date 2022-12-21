392577884980ba202d5acb09746d8da38827dc53
Guilherme Santos <sntguilherme@gmail.com>
2022-12-21 10:15:00 +0100

    standalone components

---
+++ standalone-components/src/main.ts
@@ -0,0 +1,15 @@
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { environment } from './environments/environment';
+import { AppComponent } from "./app/app.component";
+
+if (environment.production) {
+    enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        // AnalyticsService
+    ]
+}).catch(err => console.error(err));
